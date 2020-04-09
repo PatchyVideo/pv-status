4776f00af2c5a07ec923b20c55bb5449b724928d
nt_zi_mu <i@ntz.im>
2020-04-09 17:52:18 +0800

    fe&be

--- js/config.js
+++ js/config.js
@@ -1,7 +1,9 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	"m784708484-eb69a8875a3827e61da4c935", // thv
-	"m784708483-8e9a08531874492ba4af150b" // pv
+	"m784709109-cd0d287ab963940fc8c85194", // thv-fe
+	"m784708484-eb69a8875a3827e61da4c935", // thv-be
+	"m784709110-06cef0ee5a99da49dd2f0443", // pv-fe
+	"m784708483-8e9a08531874492ba4af150b" // pv-be
 ];
 
 // refresh interval (in seconds)
