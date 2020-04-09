0f302dc9667ff200f1d345157363d71fc9ae475c
nt_zi_mu <i@ntz.im>
2020-04-09 16:54:51 +0800

    own cfg

--- js/config.js
+++ js/config.js
@@ -1,18 +1,7 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'm776858319-7c829d2c3e250f85776f588e', //Community
-	'm776858325-1ddf7c7e98ce56609325db3b', //Document
-	'm782409254-3c4b19cd3f7082f404df494e', //Me
-	'm776858407-a704cd548fb5c15db5b625f9', //Update A
-	'm778126978-24e6f1ce275c2518bad1e925', //Update B
-	'm777157148-a67f71b8a807c54b8ce92c9d', //Update deliver A
-	'm777157137-ce781489d1af15253ca3f0c1', //Update deliver B
-	'm783440738-5d66a6c6630333b0268ee437', //Update deliver C
-	'm776858320-7b44f9ac136f69ac3e356e8e', //Auth A
-	'm776858321-486827042ed7a7f7b8952a05', //Auth B
-	'm778734196-a51947f061c5db10cee048f7', //Auth C
-	'm776858323-0bdafb9e9a71e3305c27c1af', //Auth D
-	'm777190463-ac0ab733e1b29c92b7de0354', //Auth E
+	"m784708484-eb69a8875a3827e61da4c935", // thv
+	"m784708483-8e9a08531874492ba4af150b" // pv
 ];
 
 // refresh interval (in seconds)
