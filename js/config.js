d8ddabff877cec28d98fa66dd374b0132125f5ad
nt_zi_mu <i@ntz.im>
2020-04-10 14:39:47 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,9 @@ var __apiKeys = [
 	"m784709109-cd0d287ab963940fc8c85194", // thv-fe
 	"m784708484-eb69a8875a3827e61da4c935", // thv-be
 	"m784709110-06cef0ee5a99da49dd2f0443", // pv-fe
-	"m784708483-8e9a08531874492ba4af150b" // pv-be
+	"m784708483-8e9a08531874492ba4af150b", // pv-be
+	"m784715864-599a7c20401cc4cf6cc27f38", // wiki-fe
+	"m784715863-3217dcfc6efb4cf23fd466ad" // wiki-be
 ];
 
 // refresh interval (in seconds)
