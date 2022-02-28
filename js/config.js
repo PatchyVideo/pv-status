a346c920c5095376c8d9cd7e781c6843a522ca82
QiroNT <i@ntz.im>
2022-02-28 20:59:37 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,8 @@ var __apiKeys = [
 	"m784715864-599a7c20401cc4cf6cc27f38", // wiki-fe
 	"m784715863-3217dcfc6efb4cf23fd466ad", // wiki-be
 	"m787659505-7b309bcdec1714c2f5d74f6f", // pt-fe
-	"m787659684-a3825629de02f534f6dc14ef" // pt-be
+	"m787659684-a3825629de02f534f6dc14ef", // pt-be
+	"m790765904-fefeb9a510c0870e5701d297", // ai-trans
 ];
 
 // refresh interval (in seconds)
